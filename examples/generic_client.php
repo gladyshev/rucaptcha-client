1c264eba79060324c35c12782aaea5366986546b
Dmitry Gladyshev <deel@email.ru>
2017-01-05 10:59:14 +0300

    GenericClient example [skip ci]

---
+++ examples/generic_client.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Rucaptcha\GenericClient;
+
+/**
+ * Anti-captcha.com API client
+ * Class Anticaptcha
+ */
+class Anticaptcha extends GenericClient
+{
+    protected $serverBaseUri = 'http://anti-captcha.com';
+}
+
+
+$captchaText = (new Anticaptcha(getenv('__ANTICAPTCHA_KEY__')))->recognizeFile(__DIR__ . '/data/captcha.png');
+
+var_dump($captchaText);
