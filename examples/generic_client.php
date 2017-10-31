5ece852e2c247723eea4d2d61129f8c9dfa2d1fa
Dmitry Gladyshev <deel@email.ru>
2017-10-31 12:56:05 +0300

    Update example doc [skip ci]

--- examples/generic_client.php
+++ examples/generic_client.php
@@ -4,9 +4,10 @@ require '../vendor/autoload.php';
 
 use Rucaptcha\GenericClient;
 
-/**
- * Anti-captcha.com API client
- * Class Anticaptcha
+/*
+ * Changes the server base URL is enough to start using anti-captcha.com API
+ *
+ * But you may to use my specific library https://github.com/gladyshev/anticaptcha-client
  */
 class Anticaptcha extends GenericClient
 {
